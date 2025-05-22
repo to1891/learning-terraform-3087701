8186bd958624b39802b7bcc5de8344f12b579ea0
to1891 <86680375+to1891@users.noreply.github.com>
2025-05-22 09:06:16 -0500

    updated vars

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner] # Bitnami
 }
 
 data "aws_vpc" "default" {
@@ -22,10 +22,10 @@ module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
-  cidr = "10.0.0.0/16"
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform = "true"
@@ -37,15 +37,15 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
   name = "blog"
-  min_size = 1
-  max_size = 2
+  min_size = var.asg_min
+  max_size = var.asg_max
   
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns = module.blog_alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
   
-  image_id                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
+  image_id      = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 }
 
 module "blog_alb" {
